b3e158a5178b981aefcba414d50bc6c1708b7e2e
Modesti Dennis <dennizmodesti@gmail.com>
2024-02-28 10:28:13 +0100

    First iteration of the transformer, yet to test

--- transformer.py
+++ transformer.py
@@ -20,7 +20,52 @@ class SmoothNorm(eqx.Module):
         norm = differentiable_l2_norm(x)
         return self.scale * (x / (norm + 1.))
     
-# Test smoothnorm is differentiable at 0
-norm = SmoothNorm(3)
-x = jnp.zeros(3)
-print(eqx.filter_value_and_grad(lambda x: jnp.sum(norm(x)))(x))
+class CausalTransformer(eqx.Module):
+    model_dim : Int[Array, ""]
+    num_heads : Int[UInt, ""]
+    attn_dropout : Float[Array, ""]
+    vocab_size : Int[UInt, ""]
+    num_layers : Int[UInt, ""]
+    hidden_dim : Int[Array, ""]
+
+    def __init__(self, model_dim : Int[Array, ""], num_heads : Int[UInt, ""], attn_dropout : Float[Array, ""], vocab_size : Int[UInt, ""], num_layers : Int[UInt, ""], hidden_dim : Int[Array, ""], *, key : PRNGKeyArray = None):
+        super().__init__()
+        self.model_dim = model_dim
+        self.num_heads = num_heads
+        self.attn_dropout = attn_dropout
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+        self.norm = SmoothNorm(model_dim)
+
+        key_mlp, key_attn, key_linears = jax.random.split(key, 3)
+        self.mlps = [eqx.nn.MLP(
+            in_size=model_dim, 
+            out_size=hidden_dim, 
+            width_size=2*hidden_dim, 
+            depth = 2,
+            use_bias=False,
+            key=key_mlp_i) for key_mlp_i in jax.random.split(key_mlp, num_layers)]
+        self.attns = [eqx.nn.MultiheadAttention(
+            num_heads=num_heads,
+            query_size=model_dim,
+            dropout_p=attn_dropout,
+            key=key_attn_i) for key_attn_i in jax.random.split(key_attn, num_layers)]
+        
+        # One linear for queries, one for keys, one for values
+        self.linears = [[eqx.nn.Linear(in_features=model_dim, 
+                                       out_features=model_dim, 
+                                       use_bias=False, 
+                                       key=key_linear_i) 
+                                       for key_linear_i in jax.random.split(key_linear, 3)] 
+                                       for key_linear in jax.random.split(key_linears, num_layers)]
+
+        def __call__(x : Float[Array, "seq dim"]) -> Float[Array, "seq dim"]:
+            for i in range(num_layers):
+                x_res = x
+                x = eqx.filter_vmap(self.norm)(x)
+                q, k, v = [eqx.filter_vmap(linear)(x) for linear in self.linears[i]]
+                attn = self.attns[i](q, k, v)
+                mlp_out = eqx.filter_vmap(self.mlps[i])(attn)
+                x = x_res + mlp_out + attn
+            return x
